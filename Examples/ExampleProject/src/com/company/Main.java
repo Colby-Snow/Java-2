261d9fdd5b7977e4682228f5f98043e76d50230a
Colby-Snow <57962827+Colby-Snow@users.noreply.github.com>
2020-02-26 14:08:13 -0600

    Notes 2/26/2020

--- Examples/ExampleProject/src/com/company/Main.java
+++ Examples/ExampleProject/src/com/company/Main.java
@@ -1,46 +1,81 @@
 package com.company;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-    static String[][] rankings = {
-            {"ivan", "King"},
-            {"Gantic", "King"},
-            {"Reton8", "King"},
-            {"AmyCline", "Queen"},
-            {"RYMM", "Prince"}
-    };
-    static int[][] points ={
-            {1115, 39720},
-            {1057, 38865},
-            {1115, 36935},
-            {1022, 30735},
-            {1033, 30550}
-    };
-
+    static String[][] rankings = new String[5][2];
+    static int[][] points = new int[5][2];
 
     public static void main(String[] args) {
-         Scanner userInput = new Scanner(System.in);
-         String iName;
-	    System.out.println("What is the player's name?");
-	    iName = userInput.next();
-	    boolean isFound = false;
+        Scanner userInput = new Scanner(System.in);
+        String iName;
+        boolean isFound = false;
         int i;
 
-	    for(i = 0; i < rankings.length; i++){
-	        if(iName.trim().toUpperCase().equals(rankings[i][0].toUpperCase())){
-	            isFound = true;
-	            break;
+        init();
+
+        iName = JOptionPane.showInputDialog("What is the player's name?");
+
+        for (i = 0; i < rankings.length; i++) {
+            if (iName.trim().toUpperCase().equals(rankings[i][0].toUpperCase())) {
+                isFound = true;
+                break;
             }
         }
-        if(isFound){
-                System.out.println( "Player " + rankings[i][0] +
+        if (isFound) {
+            JOptionPane.showMessageDialog(null,"Player " + rankings[i][0] +
                     " has reached " + rankings[i][1] + " status.\n" +
                     "Quests: " + points[i][0] + "\nAG Points :" + points[i][1]);
-        }
-        else {
-            System.out.println("Player " + iName + " not found.");
-        }
+        } else {
+            JOptionPane.showMessageDialog(null,"Player " + iName + " not found.");
         }
     }
+
+    private static void init() {
+        //build arrays from player stats input file
+        LoadArrays();
+        //other tasks
+    }
+
+    private static void LoadArrays() {
+        String record;      //holds record read from input file
+        String[] fields;    //holdsf the fields split out from the record
+        Scanner playerScanner;
+        boolean eof = false;
+        File file = new File("player_stats.dat");
+
+        try{
+            playerScanner = new Scanner(file);
+            int row = 0;    //set row index to zero
+            //file exists, but make sure its not empty
+            if(playerScanner.hasNext()){
+                //Loop thru file
+                //read a record, split it into fields, use fields to build the arrays
+                while(!eof){
+                    record = playerScanner.nextLine();
+                    fields = record.split(",");
+                    rankings[row][0] = fields[0];
+                    rankings[row][1] = fields[1];
+                    points[row][0] = Integer.parseInt(fields[2]);
+                    points[row][1] = Integer.parseInt(fields[3]);
+                    row++;
+                    if(!playerScanner.hasNext()){   //check for eof and set switch
+                        eof = true;
+                    }
+                }
+                JOptionPane.showMessageDialog(null, "Player stat file loaded ok");
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Player stat file empty, program aborting");
+                System.exit(1);
+            }
+        }
+        catch(FileNotFoundException e){
+            JOptionPane.showMessageDialog(null,"Player stat file not found, program aborting.");
+            System.exit(1);
+        }
+    }
+}
