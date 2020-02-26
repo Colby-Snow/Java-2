d5a9b87f8a1be5eeffe7f619b86cb20a1df9b959
Iceflame21 <19cmsnow@gmail.com>
2020-02-25 18:54:29 -0600

    Finished set employees method

--- Projects/Project 1/Unit 1/src/com/company/MorgansCalc.java
+++ Projects/Project 1/Unit 1/src/com/company/MorgansCalc.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class MorgansCalc {
 
     String[][] Employees = new String[10][3];
-    Double[][] PayCheckAmt = new Double[4][3];
+    Double[] PayCheckAmt = new Double[10];
     String fName,lName;
     int menuOperation;
     Scanner userInput = new Scanner(System.in);
+
 
     public void mainMenu(){
         System.out.println("Please one of the following option numbers.");
@@ -18,6 +19,7 @@ public class MorgansCalc {
         switch(menuOperation) {
             case 1:
                 System.out.println("Option 1 selected");
+                setEmployees();
                 break;
             case 2:
                 System.out.println("Option 2 selected");
@@ -32,25 +34,47 @@ public class MorgansCalc {
     }
 
     public void setEmployees(){
+        String employContinue;
+
         for(int i = 0; i <= 10; i++){
             for (int j = 0; j <= 3; j++){
+                boolean validStatus = false;
                 switch(j) {
                     case 1:
-                        System.out.println("Please enter the first name of employee number "+ i);
-                        Employees[i-1][j-1] = userInput.next();
+                        System.out.println("Please enter the first name of employee number "+ i + 1);
+                        Employees[i][j] = userInput.next();
                     case 2:
-                        System.out.println("Please enter the last name of employee number " + i);
-                        Employees[i-1][j-1] = userInput.next();
+                        System.out.println("Please enter the last name of employee number " + i + 1);
+                        Employees[i][j] = userInput.next();
                     case 3:
-                        System.out.println("Please enter the employee number " + i +"'s status");
-                        Employees[i-1][j-1] = userInput.next();
+                        do{
+                            System.out.println("Please enter the employee number " + i + 1 +"'s status full time (FT) or part time (PT)");
+                            Employees[i][j] = userInput.next();
+                            if(Employees[i][j].toUpperCase() == "PT" || Employees[i][j].toUpperCase() == "FT"){
+                                validStatus = true;
+                            }
+                            else{
+                                System.out.println("Invalid status please enter either PT for part time or FT for full time.");
+                            }
+                        }while(!validStatus);
                 }
+            }
+            System.out.println("Please enter employee number " + i + 1 +"'s paycheck amount");
+            PayCheckAmt[i] = userInput.nextDouble();
+            do{
+                System.out.println("Would you like to continue entering employees? (Y or N)");
+                employContinue = userInput.next().toUpperCase();
+            }while (employContinue != "Y" && employContinue != "N");
+            if(employContinue == "N") {
+                break;
             }
         }
     }
 
-    public String[][] SearchOption(String fName, String lName){
-        Employees[2][1] = "Hello";
-        return Employees;
+    public void displayEmployees(){
+        for (int i = 0; i<=10; i++){
+            
+        }
     }
 }
+
